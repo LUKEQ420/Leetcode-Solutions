42365daa24e64e7b971fb455f07f36a56c0742d6
JQ-Luke <167411886+LUKEQ420@users.noreply.github.com>
2026-09-18 12:05:13 +0200

    Time: 0 ms (100%), Space: 20.4 MB (79.63%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,10 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        hashmap = {}
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] not in hashmap:
-                hashmap[nums[i]] = 1
-                nums[count] = nums[i]
+        if not nums:
+            return 0
+        slow = 0
+        count = 1
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                nums[slow+1] = nums[fast]
                 count += 1
+                slow += 1
         return count
